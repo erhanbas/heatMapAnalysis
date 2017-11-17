f35a475d80288b1c2b1f699481436799a5db4b5b
winnubstj <winnubstj@janelia.hhmi.org>
2017-11-17 17:59:31 -0500

    changed dendrogram orientation

--- createPlots.m
+++ createPlots.m
@@ -26,10 +26,7 @@ end
 
 % parse file names, ugly ...
 names = {data(:).name};
-for ii=1:numfiles
-    tmp = names{ii}(1:end-4);
-    names{ii} = [tmp(3:6),tag];
-end
+names = cellfun(@(x) x(1:6),names,'UniformOutput',false);
 
 %%
 [h_data,hxyz_data,nhxyz_data] = prepareData(data);
@@ -40,35 +37,22 @@ dm = max(dminmax(:,:,1),dminmax(:,:,2));
 dm = max(dm,dm');
 
 %%
-% close all
 for type = {'jaccard'} % hist
     %%
-    % tag =  {'n0059','n0265'};
-    tag = [];
     [hpD,hZ,hcolor]=dendClust(h_data,type{1});
-    %hpD_=hpD(find(triu(hpD,1)'));
+    vizClust(hZ,hcolor,names);
 
-    vizClust(hZ,hcolor,names,tag);
-    % title(sprintf('%s-base',type{1}))
-    % export_fig(fullfile(vizfolder,sprintf('%s-%s-base.tif',experiment,type)))
-    
-    %cZ = cluster(hZ,'Cutoff',hcolor-eps,'Criterion','distance'); % based on names
-    %NumCluster = max(cZ);
-    
     % since tree there are #edge+1 clusters
     NumCluster = sum(hZ(:,3)==1)+1;
     pDupdated = pDCorrection(dm,hpD,hZ,NumCluster);
     %%
     [upZ,upcolor] = d2Z(pDupdated);
     upcolor = 1; % to preserve initial clustering result, set threshold to 1
-    [H,P] = vizClust(upZ,upcolor,names,tag);
+    [H,P] = vizClust(upZ,upcolor,names);
    
     ax = gca; % get the axes handle
     ticklabels = names(P);
     labelcolor = recolortext(H,ax,ticklabels);
     namesC=labelcolor;namesC(P,:)=labelcolor;
     title(sprintf('%s-clust',type{1}))
-    if exist('vizfolder','var')
-        export_fig(fullfile(vizfolder,sprintf('%s-%s-clust.tif',experiment,type{1})))
-    end
 end
--- vizClust.m
+++ vizClust.m
@@ -1,20 +1,12 @@
-function [H,P] = vizClust(hZ,hcolor,names,tag)
+function [H,P] = vizClust(hZ,hcolor,names)
 addpath(genpath('./export_fig'))
 % close all
 numfiles = length(names);
 figure('units','normalized','outerposition',[0.5 0 .5 1])
 set(gcf,'Color','k');
-[H,~,P] = dendrogram(hZ,numfiles,'ColorThreshold',hcolor,'labels',names);
+[H,~,P] = dendrogram(hZ,numfiles,'ColorThreshold',hcolor,'labels',names,'Orientation','right');
 set(gca,'xticklabelrotation',90);
 set(gca,'Color','k');
 set(findobj('Type','Line','Color','k'),'Color','w')
 set(gca, 'YColor', [1 1 1]*.7);
 set(gca, 'XColor', [1 1 1]*.7);
-
-if ~isempty(tag)
-    tag1 = tag{1};
-    tag2 = tag{2};
-    ix1 = find(contains(names,tag1));
-    ix2 = find(contains(names,tag2));
-    [Index1,Index2] = tagIds(tag1,tag2);
-end
